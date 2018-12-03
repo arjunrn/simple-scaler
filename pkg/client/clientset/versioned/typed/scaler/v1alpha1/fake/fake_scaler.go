18318e84f65c9c5e9a9cbed1b712fa4dd24c11fb
Arjun Naik <arjun.rn@gmail.com>
2018-12-03 19:28:13 +0100

    Fix version of code generator
    Signed-off-by: Arjun Naik <arjun.rn@gmail.com>

--- pkg/client/clientset/versioned/typed/scaler/v1alpha1/fake/fake_scaler.go
+++ pkg/client/clientset/versioned/typed/scaler/v1alpha1/fake/fake_scaler.go
@@ -131,7 +131,7 @@ func (c *FakeScalers) DeleteCollection(options *v1.DeleteOptions, listOptions v1
 // Patch applies the patch and returns the patched scaler.
 func (c *FakeScalers) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.Scaler, err error) {
 	obj, err := c.Fake.
-		Invokes(testing.NewPatchSubresourceAction(scalersResource, c.ns, name, pt, data, subresources...), &v1alpha1.Scaler{})
+		Invokes(testing.NewPatchSubresourceAction(scalersResource, c.ns, name, data, subresources...), &v1alpha1.Scaler{})
 
 	if obj == nil {
 		return nil, err
